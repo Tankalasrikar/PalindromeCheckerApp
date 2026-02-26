cd1ff78176da21530afa770cef350890acd4dcdd
Tankalasrikar <st3897@srmist.edu.in>
2026-02-26 09:23:40 +0530

    [srikar] Added UC6-Queuenstack

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,36 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
-// version 4.0
+// version 6.0
 //author Srikar
-//useCase 4: Character Array Based Palindrome check
+//useCase 6: Queue + Stack Based Palindrome check
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word: ");
+        System.out.print("Enter a string to check palindrome: ");
         String input = scanner.nextLine();
+
+
+        input = input.toLowerCase();
+
+
+        Queue<Character> queue = new LinkedList<>();
+
 
         Stack<Character> stack = new Stack<>();
 
+
         for (char c : input.toCharArray()) {
-            stack.push(c);
+            if (Character.isLetterOrDigit(c)) {
+                queue.add(c);
+                stack.push(c);
+            }
         }
+
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
 
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
 
+
         if (isPalindrome) {
-            System.out.println(input + " is a Palindrome.");
+            System.out.println("\"" + input + "\" is a Palindrome.");
         } else {
-            System.out.println(input + " is NOT a Palindrome.");
+            System.out.println("\"" + input + "\" is NOT a Palindrome.");
         }
         scanner.close();
 
